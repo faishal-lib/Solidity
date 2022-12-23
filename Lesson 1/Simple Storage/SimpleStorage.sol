d8a0dade9c23935bfcda6b6944d6bb74980aab11
Mochamad Faishal <mochamadfaishal1311@gmail.com>
2022-12-23 18:14:21 +0700

    Create SimpleStorage.sol

---
+++ Lesson 1/Simple Storage/SimpleStorage.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.6.0; 
+
+contract SimpleStorage {
+
+    uint256 favoriteNumber = 5;
+    bool favorieBool = false;
+    string favoriteString = "String";
+    int256 favoriteInt = -5;
+    address favoriteAddress = 0x28Fb43b58B27E8A6bdE552eCaF3310d2Edc7dCc9;
+    bytes32 favoriteBytes = "cat";
+
+}
